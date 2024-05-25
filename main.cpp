8791ada83ad4fd6f95ee6383514c304ffc193432
Angcroft <svenegas533@gmail.com>
2024-05-25 17:50:26 -0400

    First

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main ()
+{
+    //  Define variables
+    string operation;
+    double number1, number2;
+
+    //  Get user input
+    cout << "Enter first number: ";
+    cin >> number1;
+    cout << "Enter operation (+, -, *, /): ";
+    cin >> operation;
+    cout << "Enter second number: ";
+    cin >> number2;
+
+    return 0;
+}
