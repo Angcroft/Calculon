c2dfc85aa04b63ed5bc2c932f806d58b16dfa985
Angcroft <svenegas533@gmail.com>
2024-05-25 18:25:56 -0400

    Made the code a bit better, I guess...

--- main.cpp
+++ main.cpp
@@ -3,31 +3,79 @@
 
 using namespace std;
 
+//  Function for showing results
+void showResults(double result) {
+    cout << "The result is: " << result << endl;
+}
+
+//  Function for adding two numbers
+double addOp(double a, double b) {
+    return a + b;
+}
+
+//  Function for substracting two numbers
+double subsOp(double a, double b) {
+    return a - b;
+}
+
+//  Function for multiplicating two numbers
+double multiOp(double a, double b) {
+    return a * b;
+}
+
+//  Function for dividing two numbers with division by zero management
+double divOp(double a, double b) {
+    if (b == 0) {
+        cout << "Error: Division by zero is forbidden." << endl;
+        return NAN; //  Just NotANumber
+    }
+
+    return a / b;
+}
+
+//  Main function
 int main ()
 {
     //  Define variables
-    string operation;
+    char operation;
     double number1, number2;
 
     //  Get user input
     cout << "Enter first number: ";
-    cin >> number1;
-    cout << "Enter operation (+, -, *, /): ";
-    cin >> operation;
-    cout << "Enter second number: ";
-    cin >> number2;
+    while(!(cin >> number1)) {
+        //  Invalids if wrong input
+        cout << "Invalid. Please enter a correct input: ";
+        cin.clear();    //  Clear the input
+        cin.ignore(numeric_limits<streamsize>::max(), '/n');    //  Discard the invalid input
+    }
 
-    //  If else statement checking entered operation
-    if (operation == "+") {
-        cout << number1 + number2;
-    } else if (operation == "-") {
-        cout << number1 - number2;
-    } else if (operation == "*") {
-        cout << number1 * number2;
-    } else if (operation == "/") {
-        cout << number1 / number2;
-    } else {
-        cout << "Sorry dude, wrong input...";
+    cout << "Input the operation (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Input the second number: ";
+    while(!(cin >> number2)) {
+        //  Invalids if wrong input
+        cout << "Invalid. Please enter a correct input: ";
+        cin.clear();    //  Clear the input
+        cin.ignore(numeric_limits<streamsize>::max(), '/n');    //      Discard the invalid input
+    }
+
+    //  If operation char is correct, proceed with Switch
+    switch (operation) {
+        case '+':
+            showResults(addOp(number1, number2));
+            break;
+        case '-':
+            showResults(subsOp(number1, number2));
+            break;
+        case '*':
+            showResults(multiOp(number1, number2));
+            break;
+        case '/':
+            showResults(divOp(number1, number2));
+            break;
+        default:
+            cout << "Invalid operation. Please enter a correct operation (+, -, *, /): " << endl;
     }
 
     return 0;
